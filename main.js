82a38826717eb303a0851d42358771999f36cf2d
Ibrohimjon707 <ibrohimjonathamjonov38@gmail.com>
2025-01-20 14:43:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let Setaphor = prompt("Qizil , sariq , yashil randan birini kiriting")
+if(Setaphor ==  "Qizil"){
+    alert("To'xta")
+}else if(Setaphor ==  "Sariq"){
+    alert("Kut")
+}else if(Setaphor ==  "Yashil"){
+    alert("Yur")
+}else{
+    alert("bunday rang yo'q")
+}
